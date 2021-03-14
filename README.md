135453e8368a2acfc3154d9a9dd334bc8c934752
bluetmac <bluetmacc@gmail.com>
2021-03-14 15:52:14 +0800

    0314

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# algorithm
+### 2020-03-13
+#### 复习
+-  [1. 两数之和](https://leetcode-cn.com/problems/two-sum/)
+* [code](https://github.com/weitingyuk/LeetCode-Notes-Waiting/blob/main/2020-03-13/ReverseBetween92.java)
---
+++ leetcode/src/leetcode/Solution_03_14.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution_03_14 {
+    // 时间复杂度：O(N)，其中 N 是数组中的元素数量。对于每一个元素 x，我们可以 O(1) 地寻找 target - x。
+    // 空间复杂度：O(N)，其中 N 是数组中的元素数量。主要为哈希表的开销。
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+        return null;
+    }
+}
